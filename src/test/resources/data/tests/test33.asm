e56969a0adff487fac560996ea8589f385038eb5
Santiago Ontañón <santi.ontanon@gmail.com>
2020-07-19 23:56:50 -0700

    added a test file

---
+++ src/test/resources/data/tests/test33.asm
@@ -0,0 +1,19 @@
+; Test case:
+
+l2b:
+	ds 126
+l3a:
+	jp l2b	; <-- should be optimized to jr
+	jp l4a	; <-- should be optimized to jr
+l3b:
+	ds 127
+l4a:
+	jp l3b
+	jp l5a
+l4b:
+	ds 128
+l5a:
+
+loop:
+	jp loop ; ; <-- should be optimized to jr
+
