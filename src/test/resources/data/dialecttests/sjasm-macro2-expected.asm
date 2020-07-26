4e87891e0e7140e0972f3085657321b3f286d2a1
Santiago Ontañón <santi.ontanon@gmail.com>
2020-07-25 19:28:52 -0700

    fixed some of the tests for the new hex output

--- src/test/resources/data/dialecttests/sjasm-macro2-expected.asm
+++ src/test/resources/data/dialecttests/sjasm-macro2-expected.asm
@@ -1,16 +1,16 @@
 ; Test case: 
-	ld a, 515 & #00ff
+	ld a, #0203 & #00ff
 	ld (var), a
 	or a
     jr nz, _sjasm_reusable_1_2
-	ld a, (515 & #ff00) >> 8
+	ld a, (#0203 & #ff00) >> 8
 	ld (var), a
 _sjasm_reusable_1_2:
-	ld a, 515 & #00ff
+	ld a, #0203 & #00ff
 	ld (var), a
 	or a
     jr nz, _sjasm_reusable_1_3
-	ld a, (515 & #ff00) >> 8
+	ld a, (#0203 & #ff00) >> 8
 	ld (var), a
 _sjasm_reusable_1_3:
 loop:
--- src/test/resources/data/dialecttests/sjasm-macro3-expected.asm
+++ src/test/resources/data/dialecttests/sjasm-macro3-expected.asm
@@ -1,23 +1,23 @@
 ; Test case: 
-	ld a, (258 & #ff00) >> 8
+	ld a, (#0102 & #ff00) >> 8
 	ld (var), a
 	or a
 	jr nz, _sjasm_reusable_1_4
-	ld a, 258 & #00ff
+	ld a, #0102 & #00ff
 	ld (var), a
 _sjasm_reusable_1_4:
-	ld a, (772 & #ff00) >> 8
+	ld a, (#0304 & #ff00) >> 8
 	ld (var), a
 	or a
 	jr nz, _sjasm_reusable_1_5
-	ld a, 772 & #00ff
+	ld a, #0304 & #00ff
 	ld (var), a
 _sjasm_reusable_1_5:
-	ld a, (1286 & #ff00) >> 8
+	ld a, (#0506 & #ff00) >> 8
 	ld (var), a
 	or a
 	jr nz, _sjasm_reusable_1_6
-	ld a, 1286 & #00ff
+	ld a, #0506 & #00ff
 	ld (var), a
 _sjasm_reusable_1_6:
 loop:
