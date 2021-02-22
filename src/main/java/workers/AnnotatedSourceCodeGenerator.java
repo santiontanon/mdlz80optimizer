38b19fba0c5d43af65df4d31f399500e592f829b
Santiago Ontañón <santi.ontanon@gmail.com>
2021-02-21 23:32:24 -0500

    added a new -asm+:html flag, super useful to debug binary generation in different dialects

--- src/main/java/workers/AnnotatedSourceCodeGenerator.java
+++ src/main/java/workers/AnnotatedSourceCodeGenerator.java
@@ -10,6 +10,9 @@ import cl.MDLConfig;
 import code.CodeBase;
 import code.SourceFile;
 import code.CodeStatement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.apache.commons.lang3.tuple.Pair;
 import parser.Tokenizer;
 
 /**
@@ -20,6 +23,7 @@ public class AnnotatedSourceCodeGenerator implements MDLWorker {
 
     MDLConfig config = null;
 
+    boolean generateHTML = false;
     String outputFileName = null;
 
 
@@ -33,9 +37,8 @@ public class AnnotatedSourceCodeGenerator implements MDLWorker {
     public String docString() {
         // This string has MD tags, so that I can easily generate the corresponding documentation in github with the 
         // hidden "-helpmd" flag:        
-        return "- ```-asm+ <output file>```: (task) generates a single text file containing the original assembler code " +
-               "(with macros expanded), that includes size and time annotations at the beginning of each file " +
-               "to help with manual optimizations beyond what MDL already provides.\n";
+        return "- ```-asm+ <output file>```: (task) generates a single text file containing the original assembler code (with macros expanded), that includes size and time annotations at the beginning of each file to help with manual optimizations beyond what MDL already provides.\n"
+             + "- ```-asm+:html <output file>```: (task) acts like ```-asm+```, except that the output is in html (rendered as a table), allowing it to have some extra information.\n";
     }
 
 
@@ -43,6 +46,11 @@ public class AnnotatedSourceCodeGenerator implements MDLWorker {
     public boolean parseFlag(List<String> flags) {
         if (flags.get(0).equals("-asm+") && flags.size()>=2) {
             flags.remove(0);
+            outputFileName = flags.remove(0);
+            return true;
+        } else if (flags.get(0).equals("-asm+:html") && flags.size()>=2) {
+            flags.remove(0);
+            generateHTML = true;
             outputFileName = flags.remove(0);
             return true;
         }
@@ -58,16 +66,72 @@ public class AnnotatedSourceCodeGenerator implements MDLWorker {
             
             if (config.evaluateAllExpressions) code.evaluateAllExpressions();
             
+            // Calculate the position of each statement in the generated binary:
+            HashMap<CodeStatement, Integer> positionInBinaryMap = new HashMap<>();
+            List<Pair<CodeStatement, List<Integer>>> statementBytes = new ArrayList<>();
+            BinaryGenerator gen = new BinaryGenerator(config);
+            if (!gen.generateStatementBytes(code.getMain(), code, statementBytes)) return false;
+            int position = 0;
+            for(Pair<CodeStatement, List<Integer>> pair:statementBytes) {
+                positionInBinaryMap.put(pair.getLeft(), position);
+                position += pair.getRight().size();
+            }
+            
             try (FileWriter fw = new FileWriter(outputFileName)) {
-                for(SourceFile sf:code.getSourceFiles()) {
-                    fw.write("; ------------------------------------------------\n");
-                    fw.write("; ---- " + sf.fileName + " --------------------------------\n");
-                    fw.write("; ------------------------------------------------\n\n");
-                    fw.write("; Address  Size  Time\n");
-                    fw.write("; -------------------\n");
-                    fw.write(sourceFileString(sf, code));
-                    fw.write("\n");
+                if (generateHTML) {
+                    // write HTML header:
+                    fw.write("<head>\n");            
+                    fw.write("<title>MDL: annotated source code</title>\n");
+                    fw.write("<style>\n");
+                    fw.write("  table.sourcefile {\n");
+                    fw.write("    border: 1px solid black;\n");
+                    fw.write("    background-color: #eeeeee;\n");
+                    fw.write("    }\n");
+                    fw.write("  table.sourcefile tr td {\n");
+                    fw.write("    border-right: 1px solid black;\n");
+                    fw.write("    padding-left: 8px;\n");
+                    fw.write("    padding-right: 8px;\n");
+                    fw.write("    }\n");
+                    fw.write("  table.sourcefile tr td:last-of-type {\n");
+                    fw.write("    border: none;\n");
+                    fw.write("    background-color: #ffffff;\n");
+                    fw.write("    }\n");
+                    fw.write("</style>\n");
+                    fw.write("</head>\n");
+                    fw.write("<body>\n");
                 }
+                for(SourceFile sf:code.getSourceFiles()) {
+                    
+                    if (generateHTML) {
+                        /*
+                        fw.write("<table class=\"topblock\">\n");
+                        fw.write("<tr><td><button type=\"button\" class=\"collapsible\">"+topBlock.ID+"</button>\n");
+                        fw.write("<div class=\"content\">\n");
+                        */
+                        fw.write("<hr><br><b> Source Code file: " + sf.fileName + "</b><br>\n");
+                        fw.write("<table class=\"sourcefile\" cellspacing=\"0\">\n");
+                        fw.write("<tr><td style=\"border-bottom: 1px solid black\"><b>Address</b></td>");
+                        fw.write("<td style=\"border-bottom: 1px solid black\"><b>Position in Binary</b></td>");
+                        fw.write("<td style=\"border-bottom: 1px solid black\"><b>Size</b></td>");
+                        fw.write("<td style=\"border-bottom: 1px solid black\"><b>Timing</b></td>");
+                        fw.write("<td style=\"border-bottom: 1px solid black\"><b>Assembled</b></td>");
+                        fw.write("<td style=\"border-bottom: 1px solid black\"><b>Code</b></td></tr>\n");
+                        fw.write(sourceFileHTMLString(sf, code, positionInBinaryMap));
+                        fw.write("</table>\n");
+                        fw.write("\n");
+                    } else {
+                        fw.write("; ------------------------------------------------\n");
+                        fw.write("; ---- " + sf.fileName + " --------------------------------\n");
+                        fw.write("; ------------------------------------------------\n\n");
+                        fw.write("; Address  Size  Time\n");
+                        fw.write("; -------------------\n");
+                        fw.write(sourceFileString(sf, code));
+                        fw.write("\n");
+                    }
+                }
+                if (generateHTML) {
+                    fw.write("</body>\n");
+                }                
                 fw.flush();
             } catch (Exception e) {
                 config.error("Cannot write to file " + outputFileName + ": " + e);
@@ -113,11 +177,77 @@ public class AnnotatedSourceCodeGenerator implements MDLWorker {
             while(timeString.length() < 5) timeString = " " + timeString;
             sb.append(timeString);
             sb.append("  ");
-
             sb.append(ss.toString());
             sb.append("\n");
         }
     }
+    
+    
+    public String sourceFileHTMLString(SourceFile sf, CodeBase code, HashMap<CodeStatement, Integer> positionInBinaryMap)
+    {
+        StringBuilder sb = new StringBuilder();
+        sourceFileHTMLString(sf, sb, code, positionInBinaryMap);
+        return sb.toString();
+    }
+
+    
+    public void sourceFileHTMLString(SourceFile sf, StringBuilder sb, CodeBase code, HashMap<CodeStatement, Integer> positionInBinaryMap)
+    {
+        for (CodeStatement ss:sf.getStatements()) {
+            sb.append("<tr>");
+            Integer address = ss.getAddress(code);
+            if (address == null) {
+                sb.append("<td>????</td>");
+            } else {
+                sb.append("<td>");
+                sb.append(Tokenizer.toHexWord(address, config.hexStyle));
+                sb.append("</td>");
+            }
+            
+            Integer position = positionInBinaryMap.get(ss);
+            if (position == null) {
+                sb.append("<td></td>");
+            } else {
+                sb.append("<td>");
+                sb.append(Tokenizer.toHexWord(position, config.hexStyle));
+                sb.append("</td>");
+            }
+            
+            Integer size = ss.sizeInBytes(code, true, true, true);
+            if (size == null) {
+                config.error("Cannot evaluate the size of statement: " + ss);
+                return;
+            }
+            String sizeString = "" + (size > 0 ? size:"");
+            sb.append("<td>");
+            sb.append(sizeString);
+            sb.append("</td>");
+            String timeString = "" + ss.timeString();
+            sb.append("<td>");
+            sb.append(timeString);
+            sb.append("</td>");
+            if (ss.type == CodeStatement.STATEMENT_CPUOP) {
+                List<Integer> data = ss.op.assembleToBytes(ss, code, config);
+                if (data == null) {
+                    config.error("Cannot convert " + ss.op + " to bytes in " + ss.sl);
+                    sb.append("<td>ERROR</td>");
+                } else {
+                    sb.append("<td><pre>");
+                    for(Integer v:data) {
+                        sb.append(Tokenizer.toHex(v, 2));
+                        sb.append(" ");
+                    }
+                    sb.append("</pre></td>");
+                }
+            } else {
+                sb.append("<td></td>");
+            }            
+            sb.append("<td><pre>");
+            sb.append(ss.toString());
+            sb.append("</pre></td>");
+            sb.append("</tr>\n");
+        }
+    }    
     
     
     @Override
@@ -129,9 +259,11 @@ public class AnnotatedSourceCodeGenerator implements MDLWorker {
     @Override
     public MDLWorker cloneForExecutionQueue() {
         AnnotatedSourceCodeGenerator w = new AnnotatedSourceCodeGenerator(config);
+        w.generateHTML = generateHTML;
         w.outputFileName = outputFileName;
         
         // reset state:
+        generateHTML = false;
         outputFileName = null;
         
         return w;
--- src/main/java/workers/BinaryGenerator.java
+++ src/main/java/workers/BinaryGenerator.java
@@ -14,8 +14,10 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
 
 /**
  *
@@ -70,6 +72,20 @@ public class BinaryGenerator implements MDLWorker {
     
     public boolean writeBytes(SourceFile sf, CodeBase code, OutputStream os) throws Exception
     {
+        List<Pair<CodeStatement, List<Integer>>> statementBytes = new ArrayList<>();
+        if (!generateStatementBytes(sf, code, statementBytes)) return false;
+        for(Pair<CodeStatement, List<Integer>> pair:statementBytes) {
+            List<Integer> data = pair.getRight();
+            for(int value:data) {
+                os.write(value);
+            }
+        }
+        return true;
+    }
+    
+    
+    public boolean generateStatementBytes(SourceFile sf, CodeBase code, List<Pair<CodeStatement, List<Integer>>> statementBytes)
+    {
         for (CodeStatement ss:sf.getStatements()) {
             switch(ss.type) {
                 case CodeStatement.STATEMENT_NONE:
@@ -80,23 +96,24 @@ public class BinaryGenerator implements MDLWorker {
                     break;
 
                 case CodeStatement.STATEMENT_INCLUDE:
-                    if (!writeBytes(ss.include, code, os)) return false;
+                    if (!generateStatementBytes(ss.include, code, statementBytes)) return false;
                     break;
 
                 case CodeStatement.STATEMENT_INCBIN:
                 {
+                    List<Integer> data = new ArrayList<>();
                     int skip = 0;
                     int size = 0;
                     if (ss.incbinSkip != null) skip = ss.incbinSkip.evaluateToInteger(ss, code, false);
                     if (ss.incbinSize != null) size = ss.incbinSize.evaluateToInteger(ss, code, false);
                     try (InputStream is = new FileInputStream(ss.incbin)) {
                         while(is.available() != 0) {
-                            int data = is.read();
+                            int value = is.read();
                             if (skip > 0) {
                                 skip --;
                                 continue;
                             }
-                            os.write(data);
+                            data.add(value);
                             size --;
                             if (size <= 0) break;
                         }
@@ -104,42 +121,55 @@ public class BinaryGenerator implements MDLWorker {
                         config.error("Cannot expand incbin: " + ss.incbin);
                         return false;
                     }
+                    statementBytes.add(Pair.of(ss, data));
                     break;
                 }
 
                 case CodeStatement.STATEMENT_DATA_BYTES:
+                {
+                    List<Integer> data = new ArrayList<>();
                     for(Expression exp: ss.data) {        
-                        if (!expressionToBytes(exp, ss, code, os)) return false;
+                        if (!expressionToBytes(exp, ss, code, data)) return false;
                     }
+                    statementBytes.add(Pair.of(ss, data));
                     break;
+                }
 
                 case CodeStatement.STATEMENT_DATA_WORDS:
+                {
+                    List<Integer> data = new ArrayList<>();
                     for(Expression exp: ss.data) {
                         if (exp.evaluatesToNumericConstant()) {
                             int v = exp.evaluateToInteger(ss, code, true);
-                            os.write(v&0x00ff);
-                            os.write((v>>8)&0x00ff);
+                            data.add(v&0x00ff);
+                            data.add((v>>8)&0x00ff);
                         } else {
                             config.error("Cannot evaluate expression " + exp + "when generating a binary.");
                             return false;
                         }
                     }
+                    statementBytes.add(Pair.of(ss, data));
                     break;
+                }
 
                 case CodeStatement.STATEMENT_DATA_DOUBLE_WORDS:
+                {
+                    List<Integer> data = new ArrayList<>();
                     for(Expression exp: ss.data) {
                         if (exp.evaluatesToNumericConstant()) {
                             int v = exp.evaluateToInteger(ss, code, true);
-                            os.write(v&0x00ff);
-                            os.write((v>>8)&0x00ff);
-                            os.write((v>>16)&0x00ff);
-                            os.write((v>>24)&0x00ff);
+                            data.add(v&0x00ff);
+                            data.add((v>>8)&0x00ff);
+                            data.add((v>>16)&0x00ff);
+                            data.add((v>>24)&0x00ff);
                         } else {
                             config.error("Cannot evaluate expression " + exp + "when generating a binary.");
                             return false;
                         }
                     }
+                    statementBytes.add(Pair.of(ss, data));
                     break;
+                }
 
                 case CodeStatement.STATEMENT_DEFINE_SPACE:
                     if (ss.space_value != null) {
@@ -153,7 +183,11 @@ public class BinaryGenerator implements MDLWorker {
                             config.error("Cannot evaluate " + ss.space + " in " + ss.sl);
                             return false;
                         }
-                        for(int i = 0;i<amount;i++) os.write(value);
+                        List<Integer> data = new ArrayList<>();
+                        for(int i = 0;i<amount;i++) {
+                            data.add(value);
+                        }
+                        statementBytes.add(Pair.of(ss, data));
                     }
                     break;
 
@@ -164,9 +198,7 @@ public class BinaryGenerator implements MDLWorker {
                         config.error("Cannot convert op " + ss.op + " to bytes!");
                         return false;
                     }
-                    for(Integer value: data) {
-                        os.write(value);
-                    }
+                    statementBytes.add(Pair.of(ss, data));
                     break;
                 }
             }
@@ -176,33 +208,33 @@ public class BinaryGenerator implements MDLWorker {
     }
     
     
-    public boolean expressionToBytes(Expression exp, CodeStatement ss, CodeBase code, OutputStream os) throws Exception 
+    public boolean expressionToBytes(Expression exp, CodeStatement ss, CodeBase code, List<Integer> data)
     {
         Object val = exp.evaluate(ss, code, true);
         if (val == null) {
             config.error("Cannot evaluate expression " + exp + "when generating a binary.");
             return false;
         }
-        return valueToBytes(val, ss, code, os);
+        return valueToBytes(val, ss, code, data);
     }
     
     
-    public boolean valueToBytes(Object val, CodeStatement ss, CodeBase code, OutputStream os) throws Exception 
+    public boolean valueToBytes(Object val, CodeStatement ss, CodeBase code, List<Integer> data)
     {
         if (val instanceof Integer) {
             int v = (Integer)val;
-            os.write(v&0x00ff);
+            data.add(v&0x00ff);
         } else if (val instanceof String) {
             String v = (String)val;
             for(int i = 0;i<v.length();i++) {
-                os.write(v.charAt(i));
+                data.add((int)v.charAt(i));
             }
         } else if (val instanceof List) {
             for(Object val2:(List)val) {
-                if (!valueToBytes(val2, ss, code, os)) return false;
+                if (!valueToBytes(val2, ss, code, data)) return false;
             }
         } else if (val instanceof Expression) {
-            return expressionToBytes((Expression)val, ss, code, os);
+            return expressionToBytes((Expression)val, ss, code, data);
         } else {
             config.error("Unsupported value " + val + "when generating a binary.");
             return false;
