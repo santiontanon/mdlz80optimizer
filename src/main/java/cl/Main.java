478b67cf313940a1d38996dba1b2332861ec54ef
Santiago Ontañón <santi@Santiagos-MacBook-Pro.local>
2021-05-24 21:50:21 -0400

    changed version number to 2.1, getting ready for final testing and relese of the new version

--- src/main/java/cl/Main.java
+++ src/main/java/cl/Main.java
@@ -17,7 +17,7 @@ import workers.searchopt.SearchBasedOptimizer;
 
 public class Main {
     
-    public static String VERSION_STRING = "v2.1dev";
+    public static String VERSION_STRING = "v2.1";
 
     public static void main(String args[]) throws Exception {
         // Set up the MDL configuration:
