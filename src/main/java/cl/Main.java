e40a4dd76129548baba9cb548ddcfc290f0f5ef8
Santiago Ontañón <santi@Santiagos-MacBook-Pro.local>
2022-04-06 08:16:11 -0400

    changed version name from 2.4dev to 2.4

--- src/main/java/cl/Main.java
+++ src/main/java/cl/Main.java
@@ -20,7 +20,7 @@ import workers.searchopt.SearchBasedOptimizer;
 
 public class Main {
     
-    public static String VERSION_STRING = "v2.4dev";
+    public static String VERSION_STRING = "v2.4";
 
     public static void main(String args[]) throws Exception {
         // Set up the MDL configuration:
